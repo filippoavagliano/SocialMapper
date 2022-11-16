937f270669b6e39b9fa831b7a70bf0849e5be30e
Filippo Avagliano <filippo.avagliano@gmail.com>
2022-11-16 17:02:38 +0100

    .

---
+++ advertisingTemplate.py
@@ -0,0 +1,22 @@
+import json
+
+f = open('result.json')
+
+profile = json.load(f)
+
+logos = {}
+
+logos = profile['logohunter']
+
+logosSorted = sorted(logos.items(), key = lambda x : x[1],reverse=True)
+converted_dict = dict(logosSorted)
+print(converted_dict)
+
+'''
+for i in profile['logohunter']:
+    #print(i)
+    logos.append(i)
+
+print(logos)
+'''
+f.close()
