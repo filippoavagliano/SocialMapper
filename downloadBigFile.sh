b9bdfa2c9bf422ee3adff13d21c3da8c9d66b148
Filippo Avagliano <filippo.avagliano@gmail.com>
2022-11-20 22:17:40 +0100

    bash script v2

--- downloadBigFile.sh
+++ downloadBigFile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd carrecognition
 wget "https://drive.google.com/uc?id=12deopnyKW8ZCX9cFCS3Ov_uPVfOO1qEf&export=download&confirm=t" -O "models.zip"
-unzip models.zip
+unzip models.zip -d models
 
 cd ../logohunter
 wget "https://drive.google.com/uc?id=13NDUmo8FJv-HMYBzwj1twKxnvcDXbD92&export=download&confirm=t
 " -O "data.zip"
-unzip models.zip -d data
+unzip data.zip -d data
 
 cd src
 wget "https://drive.google.com/uc?id=13XjHDEIc3tYR9CenDUAbvZfKxovEy_58&export=download&confirm=t" -O "srcFile.zip"
