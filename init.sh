85e08505f9a8021dba197627f5c681801ce5e005
Chiara <c.menniti@studenti.unisa.it>
2022-11-23 16:16:33 +0100

    update init

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ wget "https://drive.google.com/uc?id=12deopnyKW8ZCX9cFCS3Ov_uPVfOO1qEf&export=do
 unzip models.zip -d models
 
 cd ../logohunter
-wget "https://drive.google.com/uc?id=13NDUmo8FJv-HMYBzwj1twKxnvcDXbD92&export=download&confirm=t
-" -O "data.zip"
-unzip data.zip -d data
+mkdir data
+cd data
+wget "https://drive.google.com/uc?id=1TL-_zg81Ik3pqvM237wwYPEM1GjhjLL7&export=download&confirm=t" -O "data.zip"
+unzip data.zip -d test
 
 cd src
 wget "https://drive.google.com/uc?id=13XjHDEIc3tYR9CenDUAbvZfKxovEy_58&export=download&confirm=t" -O "srcFile.zip"
@@ -15,7 +16,6 @@ unzip srcFile.zip
 cd keras_yolo3
 wget "https://drive.google.com/uc?id=13aahSd1Qxp69VDbUCWzge1hf7GwMUvXF&export=download&confirm=t" -O "keras_yolo_file.zip"
 unzip keras_yolo_file.zip
-
 
 
 
