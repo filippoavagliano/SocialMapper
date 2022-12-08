3b5b9eb6cd253e4c2e2681554494a80e7dd81a70
c <c>
2022-12-08 12:55:15 +0100

    remove log

--- output_scripts/logohunter_output.py
+++ output_scripts/logohunter_output.py
@@ -2,7 +2,6 @@ import os
 
 
 def logohunter_output(logohunter_output_path):
-    print(logohunter_output_path)
     logo_dict = {}
     logo_output_path = os.path.join(logohunter_output_path, 'out.txt')
     data_array = [line.strip() for line in open(logo_output_path)]
